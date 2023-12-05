4b6a2500777f6372a4d97f837ee1c3e1b4c76be0
wendibianca <wendi.bianca@gmail.com>
2023-12-05 22:15:53 +0000

    Desafio Felipão

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("olá");
+const nomeHeroi = "Yudi";
+let experienciaHeroi = 90001;
+let nivelHeroi = "";
+
+if (experienciaHeroi <= 1000) {
+    nivelHeroi = "Ferro";
+} else if (experienciaHeroi > 1000 && experienciaHeroi <= 2000) {
+    nivelHeroi = "Bronze";
+} else if (experienciaHeroi > 2000 && experienciaHeroi <= 5000) {
+    nivelHeroi = "Prata";
+} else if (experienciaHeroi > 5000 && experienciaHeroi <= 7000) {
+    nivelHeroi = "Ouro";
+} else if (experienciaHeroi > 7000 && experienciaHeroi <= 8000) {
+    nivelHeroi = "Platina";
+} else if (experienciaHeroi > 8000 && experienciaHeroi <= 9000) {
+    nivelHeroi = "Ascendente";
+} else if (experienciaHeroi > 9000 && experienciaHeroi <= 10000) {
+    nivelHeroi = "Imortal";
+} else if (experienciaHeroi > 10000) {
+    nivelHeroi = "Radiante";
+}
+
+console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivelHeroi + "!");
